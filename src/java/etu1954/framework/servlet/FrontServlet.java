8a79a7495cd581b5bbbeb86cf0440502c44e02cc
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2023-03-21 13:47:59 +0300

    sprint3

--- src/java/etu1954/framework/servlet/FrontServlet.java
+++ src/java/etu1954/framework/servlet/FrontServlet.java
@@ -5,24 +5,34 @@
  */
 package etu1954.framework.servlet;
 
-<<<<<<<HEAD
-
+import annotation.MyUrl.MyURL;
 import etu1954.framework.Mapping;
-import java.util.HashMap;=======
+import java.util.HashMap;
 import java.io.IOException;
-import java.io.PrintWriter;>>>>>>>main
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
 
 /**
  *
  * @author ITU
  */
 public class FrontServlet extends HttpServlet {
-<<<<<<< HEAD
     HashMap<String, Mapping> mappingUrls;
+    
+ public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        HashMap<String,Mapping> mappingUrls = this.getMappings();
+        this.setMappingUrls(mappingUrls);
+    }
     
     public void setMappingUrls(HashMap<String, Mapping> mappingUrls) {
         this.mappingUrls = mappingUrls;
@@ -31,8 +41,7 @@ public class FrontServlet extends HttpServlet {
     public HashMap<String, Mapping> getMappingUrls() {
         return this.mappingUrls;
     }
-=======
->>>>>>> main
+
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -59,6 +68,39 @@ public class FrontServlet extends HttpServlet {
             out.println("</html>");
         }
     }
+    
+       @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       // processRequest(request, response);
+          response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1>Liste des mappings :</h1>");
+            for (Map.Entry<String, Mapping> entry : mappingUrls.entrySet()) {
+               out.println("<p>URL: " + entry.getKey() + "</p>");
+               out.println("<p>Class: " + entry.getValue().getClassName() + "</p>");
+               out.println("<p>Method: " + entry.getValue().getMethod() + "</p><br>");
+            }
+            out.println("</body></html>");
+    }
+    
+    public static HashMap<String, Mapping> getMappings() {
+       HashMap<String, Mapping> mappingUrls = new HashMap<>();
+       Reflections reflections = new Reflections("Models", new MethodAnnotationsScanner());
+       Set<Method> methods = reflections.getMethodsAnnotatedWith(MyURL.class);
+       for (Method method : methods) {
+           Mapping tempMapping = new Mapping();
+           tempMapping.setClassName(method.getDeclaringClass().getSimpleName());
+           tempMapping.setMethod(method.getName());
+
+           MyURL url = method.getAnnotation(MyURL.class);
+           String key = url.url();
+           mappingUrls.put(key, tempMapping);
+       }
+       return mappingUrls;
+}
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -69,11 +111,7 @@ public class FrontServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+
 
     /**
      * Handles the HTTP <code>POST</code> method.
