1fbfb395d41433d502d00b1cd9af4a0ec9f570dd
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2023-03-27 06:38:42 +0100

    create JAR & WAR

--- build.bat
+++ build.bat
@@ -1,18 +1,21 @@
 javac -d Framework/build/web/WEB-INF/classes framework/src/java/etu1954/framework/*.java framework/src/java/etu1954/framework/annotation/*.java
+jar cvf framework.jar -C .\Framework\build\web\WEB-INF\classes\ .
+jar cvf test_framework.war -C .\Test_Framework\build\web\WEB-INF\ .
 
 jar cvf Framework/build/web/WEB-INF/lib/framework.jar -C Framework/build/web/WEB-INF/classes/ .
 
-@REM cd Test_Framework
+cd Test_Framework
 
-@REM mkdir build
+mkdir build
 
-@REM mkdir build/web
+mkdir build/web
 
-@REM mkdir build\web\WEB-INF\lib
-@REM mkdir build\web\WEB-INF\classes
+mkdir build\web\WEB-INF\lib
+mkdir build\web\WEB-INF\classes
 
-@REM cd ..
+cd ..
 
-copy Framework\build\web\WEB-INF\lib\framework.jar Test_Framework\build\web\WEB-INF\lib\
+copy framework.jar Test_Framework\build\web\WEB-INF\lib\
+copy test_framework.war "C:\Program Files\Apache Software Foundation\Apache Tomcat 8.0.27\webapps\"
+
 javac -d Test_Framework/build/web/WEB-INF/classes Test_Framework/src/java/Models/*.java 
-
